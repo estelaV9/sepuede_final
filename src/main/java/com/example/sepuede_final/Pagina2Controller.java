e24e9c3256b2929ac389539ab91fc0440b576cb3
estelaV9 <esteladeveg@gmail.com>
2023-06-10 11:49:20 +0200

    añadir controladores
    funciona el login

--- src/main/java/com/example/sepuede_final/Pagina2Controller.java
+++ src/main/java/com/example/sepuede_final/Pagina2Controller.java
@@ -50,11 +50,52 @@ public class Pagina2Controller {
 }
 
     @FXML
+    void AccionNocuenta(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena = (Stage) source.getScene().getWindow();
+        escena.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("general.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            generalController controller = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.setTitle("Pagina2");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
     void AccinoCancelar(ActionEvent event) {
         txtContraseya.setText("");
         txtUsuario.setText("");
     }
-
+    @FXML
+    void AccionRegistrarseButton(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena2 = (Stage) source.getScene().getWindow();
+        escena2.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registrar.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            registrarController controller = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.setTitle("registrarse");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     @FXML
@@ -87,15 +128,32 @@ public class Pagina2Controller {
 
     @FXML
     void AccionLogin(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena1 = (Stage) source.getScene().getWindow();
+        escena1.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Pagina2.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            Pagina2Controller controller = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.setTitle("log in");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @FXML
     void AccionRegistrarse(ActionEvent event) {
         Node source = (Node) event.getSource();
-        Stage escena = (Stage) source.getScene().getWindow();
-        escena.close();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registrarse_pantalla.fxml"));
+        Stage escena2 = (Stage) source.getScene().getWindow();
+        escena2.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registrar.fxml"));
         try {
             Parent root = fxmlLoader.load();
             registrarController controller = fxmlLoader.getController();
--- src/main/java/com/example/sepuede_final/registrarController.java
+++ src/main/java/com/example/sepuede_final/registrarController.java
@@ -1,4 +1,119 @@
 package com.example.sepuede_final;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class registrarController {
+
+    @FXML
+    private PasswordField txtContrasenya;
+    @FXML
+    private TextField txtUusario;
+
+    @FXML
+    void AccionCancelar2(ActionEvent event) {
+        txtContrasenya.setText("");
+        txtUusario.setText("");
+    }
+
+    @FXML
+    void AccionCerrar2(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena = (Stage) source.getScene().getWindow();
+        escena.close();
+    }
+
+    @FXML
+    void AccionLogin2(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena1 = (Stage) source.getScene().getWindow();
+        escena1.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Pagina2.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            Pagina2Controller controller = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.setTitle("log in");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void AccionSign(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena1 = (Stage) source.getScene().getWindow();
+        escena1.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("general.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            generalController controller = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.setTitle("Pagina General");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void AccionSincuenta2(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena = (Stage) source.getScene().getWindow();
+        escena.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("general.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            generalController controller = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.setTitle("Pagina2");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void AccionVolver2(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage escena = (Stage) source.getScene().getWindow();
+        escena.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("open.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            OpenController controller = fxmlLoader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.sizeToScene();
+            stage.setTitle("Inicio");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
